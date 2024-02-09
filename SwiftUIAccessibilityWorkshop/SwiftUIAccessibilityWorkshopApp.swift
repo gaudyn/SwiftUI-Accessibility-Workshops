c25ea6d67a81a5cc09eff129256633d20cd20627
Gniewomir Gaudyn <gniewomir.gaudyn@allegro.pl>
2024-02-09 13:06:28 +0100

    Initial Commit

---
+++ SwiftUIAccessibilityWorkshop/SwiftUIAccessibilityWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccessibilityWorkshopApp.swift
+//  SwiftUIAccessibilityWorkshop
+//
+//  Created by Gniewomir Gaudyn on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccessibilityWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
